1407305dd5f7a74f3d2aa8d2fd0cb9357f0b97aa
cjihrig <cjihrig@gmail.com>
2021-07-07 00:53:00 -0400

    add string length validation

--- test/index.js
+++ test/index.js
@@ -63,6 +63,108 @@ describe('ValidateDirective', () => {
       });
       Assert.strictEqual(result.data.validate, null);
     });
+
+    it('length()', async () => {
+      const server = createServer();
+      const query = `
+        query Validate($length: String) {
+          validate(length: $length)
+        }`;
+      let result = await server.executeOperation({
+        query,
+        variables: { length: 'four' }
+      });
+
+      Assert.strictEqual(result.errors, undefined);
+      Assert.strictEqual(result.data.validate, true);
+
+      result = await server.executeOperation({
+        query,
+        variables: { length: 'seven' }
+      });
+
+      Assert.deepStrictEqual(result.errors[0].extensions.exception.details[0], {
+        context: {
+          encoding: 'utf8',
+          key: 'length',
+          label: 'length',
+          limit: 4,
+          value: 'seven'
+        },
+        message: '"length" length must be 4 characters long',
+        path: ['length'],
+        type: 'string.length'
+      });
+      Assert.strictEqual(result.data.validate, null);
+    });
+
+    it('maxLength()', async () => {
+      const server = createServer();
+      const query = `
+        query Validate($maxLength: String) {
+          validate(maxLength: $maxLength)
+        }`;
+      let result = await server.executeOperation({
+        query,
+        variables: { maxLength: 'two' }
+      });
+
+      Assert.strictEqual(result.errors, undefined);
+      Assert.strictEqual(result.data.validate, true);
+
+      result = await server.executeOperation({
+        query,
+        variables: { maxLength: 'seven' }
+      });
+
+      Assert.deepStrictEqual(result.errors[0].extensions.exception.details[0], {
+        context: {
+          encoding: 'utf8',
+          key: 'maxLength',
+          label: 'maxLength',
+          limit: 3,
+          value: 'seven'
+        },
+        message: '"maxLength" length must be less than or equal to 3 characters long',
+        path: ['maxLength'],
+        type: 'string.max'
+      });
+      Assert.strictEqual(result.data.validate, null);
+    });
+
+    it('minLength()', async () => {
+      const server = createServer();
+      const query = `
+        query Validate($minLength: String) {
+          validate(minLength: $minLength)
+        }`;
+      let result = await server.executeOperation({
+        query,
+        variables: { minLength: 'four' }
+      });
+
+      Assert.strictEqual(result.errors, undefined);
+      Assert.strictEqual(result.data.validate, true);
+
+      result = await server.executeOperation({
+        query,
+        variables: { minLength: 'one' }
+      });
+
+      Assert.deepStrictEqual(result.errors[0].extensions.exception.details[0], {
+        context: {
+          encoding: 'utf8',
+          key: 'minLength',
+          label: 'minLength',
+          limit: 4,
+          value: 'one'
+        },
+        message: '"minLength" length must be at least 4 characters long',
+        path: ['minLength'],
+        type: 'string.min'
+      });
+      Assert.strictEqual(result.data.validate, null);
+    });
   });
 
   describe('GraphQL Compatibility', () => {
@@ -96,6 +198,9 @@ function createServer () {
         alphanum: String @validate(alphanum: TRUE)
         base64: String @validate(base64: { paddingRequired: false })
         case: String @validate(case: UPPER)
+        length: String @validate(length: { limit: 4, encoding: "utf8" })
+        maxLength: String @validate(maxLength: { limit: 3, encoding: "utf8" })
+        minLength: String @validate(minLength: { limit: 4, encoding: "utf8" })
       ): Boolean
       nonNullable (
         foo: String! @validate(case: LOWER)
